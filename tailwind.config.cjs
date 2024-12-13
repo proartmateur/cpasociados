9da11c2a181453b0f3e2fb803dd4ad9fe3167399
proartmateur <ennimandroid@gmail.com>
2024-12-13 05:10:55 -0600

    Home

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,11 +4,16 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        "cp-blue-0": "#EAF7FE",
+        "cp-blue-10": "#B0E4FF",
+        "cp-blue-20": "#73CFFF",
+        "cp-blue-90": "#081B3D",
+      },
       fontFamily: {
         sans: [
-          "Bricolage Grotesque Variable",
-          "Inter Variable",
-          "Inter",
+          "Fira Sans Variable",
+          "Fira Sans",
           ...defaultTheme.fontFamily.sans,
         ],
       },
